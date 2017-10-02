9ac76efe22d809a861e27e755677bf05d13a8cfb
hova <hova__0@hotmail.com>
2017-10-02 11:15:11 -0500

    Initial add

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fixsubs
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: fixsubs filename <dictionaryfile>");
+                Console.WriteLine("Return Error Codes:");
+                Console.WriteLine("1        : Input file does not exist");
+                Console.WriteLine("2        : Word Dictionary file does not exist");
+                Console.WriteLine("-1       : Abnormal termination");
+                return 0;
+            }
+            if (args.Length > 0 && !File.Exists(args[0]))
+            {
+                Console.WriteLine("File does not exist!");
+                return 1;
+            }
+            string defaultwordlist = "english-words.10";
+            if (args.Length > 1 )
+            {
+                defaultwordlist = args[1];
+            }
+            if (!File.Exists(defaultwordlist))
+            {
+                Console.WriteLine(String.Format("Default word list file {0} not found!", defaultwordlist));
+                return 2;
+            }
+
+            HashSet<string> wordlist = new HashSet<string>();
+
+
+
+                return -1;
+        }
+    }
+}
